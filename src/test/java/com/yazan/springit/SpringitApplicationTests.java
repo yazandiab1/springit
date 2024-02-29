e6e9af6b3d4a80a9e7f7fd804bedba01234dba7e
Yazan Diab <yazandiab767@gmail.com>
2024-02-29 10:04:04 +0200

    first commit for initlization repository

---
+++ src/test/java/com/yazan/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazan.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
